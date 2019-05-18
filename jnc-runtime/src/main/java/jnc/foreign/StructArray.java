ca7c2b0195d14837ee7af1e5ad26dcbca861dbdd
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-19 04:38:37 +0800

    Add class StructArray

---
+++ jnc-runtime/src/main/java/jnc/foreign/StructArray.java
@@ -0,0 +1,54 @@
+package jnc.foreign;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import javax.annotation.Nonnull;
+
+/**
+ * A utility class, should only be used in parameter
+ */
+@SuppressWarnings("unused")
+public final class StructArray<T extends Struct> extends Struct implements Iterable<T> {
+
+    private final List<T> list;
+
+    public StructArray(Supplier<T> constructor, int length) {
+        Objects.requireNonNull(constructor, "constructor");
+        @SuppressWarnings("LocalVariableHidesMemberVariable")
+        List<T> list = new ArrayList<>(length);
+        for (int i = 0; i < length; i++) {
+            list.add(inner(constructor.get()));
+        }
+        this.list = list;
+    }
+
+    public int length() {
+        return list.size();
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    @Nonnull
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        list.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return list.spliterator();
+    }
+
+}
