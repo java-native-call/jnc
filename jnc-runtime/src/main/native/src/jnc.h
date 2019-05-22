f3e235374ca94414e7be88627e0ef6597acd33ee
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-22 09:43:54 +0800

    Convert value in likely, unlikely to boolean

--- jnc-runtime/src/main/native/src/jnc.h
+++ jnc-runtime/src/main/native/src/jnc.h
@@ -98,8 +98,8 @@ do {                                                            \
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 #ifdef __GNUC__
-#define likely(x) __builtin_expect((x), 1)
-#define unlikely(x) __builtin_expect((x), 0)
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
