2abf24fc1e71f93d9915fedace05929c6bd889a1
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-24 01:10:22 +0800

    Add method ForeignProvider.getDefault()

--- jnc-runtime/src/main/java/jnc/foreign/ForeignProvider.java
+++ jnc-runtime/src/main/java/jnc/foreign/ForeignProvider.java
@@ -1,8 +1,14 @@
 package jnc.foreign;
 
 import javax.annotation.Nonnull;
+import jnc.foreign.internal.DefaultForeignProvider;
 
 public interface ForeignProvider {
+
+    @Nonnull
+    static ForeignProvider getDefault() {
+        return DefaultForeignProvider.getInstance();
+    }
 
     @Nonnull
     Foreign getForeign();
--- jnc-runtime/src/main/java/jnc/foreign/ForeignProviders.java
+++ jnc-runtime/src/main/java/jnc/foreign/ForeignProviders.java
@@ -1,13 +1,12 @@
 package jnc.foreign;
 
 import javax.annotation.Nonnull;
-import jnc.foreign.internal.DefaultForeignProvider;
 
 public final class ForeignProviders {
 
     @Nonnull
     public static Foreign getDefault() {
-        return DefaultForeignProvider.getInstance().getForeign();
+        return ForeignProvider.getDefault().getForeign();
     }
 
     private ForeignProviders() {
