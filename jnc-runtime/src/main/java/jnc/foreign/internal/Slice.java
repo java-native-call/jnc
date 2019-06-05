2065c7da02bce2fef3ca1b41afaef37ca49fb148
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-06-06 06:23:39 +0800

    Rename {size=>capacity}

--- jnc-runtime/src/main/java/jnc/foreign/internal/Slice.java
+++ jnc-runtime/src/main/java/jnc/foreign/internal/Slice.java
@@ -8,8 +8,8 @@ final class Slice extends SizedDirectMemory {
     private final Memory outer;
     private final int offset;
 
-    Slice(Memory outer, int offset, int size) {
-        super(outer.address() + offset, size);
+    Slice(Memory outer, int offset, int capacity) {
+        super(outer.address() + offset, capacity);
         this.outer = outer;
         this.offset = offset;
     }
@@ -17,7 +17,7 @@ final class Slice extends SizedDirectMemory {
     @Nonnull
     @Override
     public final Slice slice(int beginIndex, int endIndex) {
-        checkSliceRange(size(), beginIndex, endIndex);
+        checkSliceRange(capacity(), beginIndex, endIndex);
         return new Slice(outer, offset + beginIndex, endIndex - beginIndex);
     }
 
