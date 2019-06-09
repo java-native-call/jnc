1cb1ec37841d74badb246e4d8b3e306c36f5edd6
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-06-09 23:18:35 +0800

    Remove useless native methods.

--- libjnc/src/load.cpp
+++ libjnc/src/load.cpp
@@ -1,45 +1,8 @@
 #include "jnc.h"
 
-#ifdef _WIN32
-#include <windows.h>
-#else /* _WIN32 */
-
-#include <unistd.h>
-#endif
-static jint _page_size;
-
 EXTERNC JNIEXPORT jint JNICALL JNI_OnLoad
 (JavaVM *UNUSED(vm), void *UNUSED(reserved)) {
-#ifdef _WIN32
-    SYSTEM_INFO si;
-    GetSystemInfo(&si);
-    _page_size = si.dwPageSize;
-#else
-    _page_size = sysconf(_SC_PAGESIZE);
-#endif
     return JNI_VERSION_1_6;
-}
-
-/*
- * Class:     jnc_provider_NativeMethods
- * Method:    getJniVersion
- * Signature: ()I
- */
-EXTERNC JNIEXPORT jint JNICALL
-Java_jnc_provider_NativeMethods_getJniVersion
-(JNIEnv *env, jobject UNUSED(self)) {
-    return env->GetVersion();
-}
-
-/*
- * Class:     jnc_provider_NativeMethods
- * Method:    pageSize
- * Signature: ()I
- */
-EXTERNC JNIEXPORT jint JNICALL
-Java_jnc_provider_NativeMethods_pageSize
-(JNIEnv *UNUSED(env), jobject UNUSED(self)) {
-    return _page_size;
 }
 
 EXTERNC JNIEXPORT void JNICALL JNI_OnUnload
