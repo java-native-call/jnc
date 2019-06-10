c99f6e9b28a46bd687bde520d7ead34df85fb739
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-06-10 12:26:53 +0800

    Fix onUnload issue
     see 8aecc18b6216f30d59810146034fc1b7d6936f73

--- libjnc/src/load.cpp
+++ libjnc/src/load.cpp
@@ -9,7 +9,7 @@ EXTERNC JNIEXPORT void JNICALL JNI_OnUnload
 (JavaVM *vm, void *UNUSED(reserved)) {
     JNIEnv *env;
     if (likely(vm->GetEnv((void **) &env, JNI_VERSION_1_6) == JNI_OK)) {
-        jclass type = env->FindClass("jnc/foreign/internal/NativeMethods");
+        jclass type = env->FindClass("jnc/provider/NativeMethods");
         if (unlikely(type == nullptr)) {
             env->ExceptionClear();
             return;
