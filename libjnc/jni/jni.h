b1c127a8efde28840d049cc630c900249d48433e
zhanhb <6323014+zhanhb@users.noreply.github.com>
2022-01-16 19:04:28 +0800

    Update jni.h

--- libjnc/jni/jni.h
+++ libjnc/jni/jni.h
@@ -1851,6 +1851,20 @@ struct JNIEnv_ {
 #endif /* __cplusplus */
 };
 
+/*
+ * optionString may be any option accepted by the JVM, or one of the
+ * following:
+ *
+ * -D<name>=<value>          Set a system property.
+ * -verbose[:class|gc|jni]   Enable verbose output, comma-separated. E.g.
+ *                           "-verbose:class" or "-verbose:gc,class"
+ *                           Standard names include: gc, class, and jni.
+ *                           All nonstandard (VM-specific) names must begin
+ *                           with "X".
+ * vfprintf                  extraInfo is a pointer to the vfprintf hook.
+ * exit                      extraInfo is a pointer to the exit hook.
+ * abort                     extraInfo is a pointer to the abort hook.
+ */
 typedef struct JavaVMOption {
     char *optionString;
     void *extraInfo;
